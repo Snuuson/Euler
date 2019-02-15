09d9af0487b3758589ae7d14a70541739df5c2f8
Snuuson <Snuuson@DESKTOP-324OP39>
2019-02-15 15:49:53 +0100

    Completed Problem 11

--- Euler/Program.cs
+++ Euler/Program.cs
@@ -13,10 +13,12 @@ namespace Euler
             Problem8 p8 = new Problem8();
             Problem9 p9 = new Problem9();
             Problem10 p10 = new Problem10();
+            Problem11 p11 = new Problem11();
 
             p8.P();
             p9.P();
             p10.P();
+            p11.P();
             Console.WriteLine("-End-");
             Console.ReadKey();
         }
